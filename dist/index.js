59ccf09efcc027c0fee6cf64a2931d010ebc1342
EvheniiDovhui <edovguy97@gmail.com>
2024-03-12 12:01:20 +0200

    hw 1

---
+++ dist/index.js
@@ -0,0 +1,47 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(1, 1);
+// function add(num1: number, num2: number): string {
+// 	return `${num1}` + `${num2}`
+// }
+// console.log(add(1, 2))
+// enum Sizes {
+// 	small = 'small',
+// 	medium = 'medium',
+// 	large = 'large',
+// }
+// const button: Sizes = Sizes.medium
+// let name: unknown = 'John'
+// name = 20
+// name.toFixed()
+// const age: any = 10
+// age = '20'
+// age = true
+// let total: number = 100
+// let name: string = 'John'
+// let isActive: boolean = false
+// let empty: null = null
+// let notInitialize: undefined = undefined
+// let age = 10
+// age = '20'
+// const number: number[] = [1, 1, 2, 3, 4, 5, 6]
+// number.push('7')
+// type User = { name: string; age: number }
+// const user: User = {
+// 	name: 'John',
+// 	age: 20,
+// }
+// type eventType = 'lessonStart' | 'lessonEnd'
+// const event: eventType = 'lessonStart'
+// type size = 'S' | 'M' | 'L'
+// const size: size = 'S'
+// function add(num1: number, num2: number): number {
+// 	return num1 + num2
+// }
+// add(1, 1)
+// let total: number = 100
+// let name: string = 'John'
+// let isActive: boolean = false
+// let empty: null = null
+// let notInitialize: undefined = undefined
